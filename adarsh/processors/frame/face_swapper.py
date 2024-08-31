e8063a814ecdb13ac3a502386b2cdb849bee3fb2
adarsh342 <126852443+adarsh342@users.noreply.github.com>
2024-08-31 13:48:28 +0530

    Update face_swapper.py

--- adarsh/processors/frame/face_swapper.py
+++ adarsh/processors/frame/face_swapper.py
@@ -22,7 +22,7 @@ def get_face_swapper() -> Any:
     with THREAD_LOCK:
         if FACE_SWAPPER is None:
             model_path = resolve_relative_path('../models/inswapper_128.onnx')
-            FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=adarsh.globals.execution_providers)
+            FACE_SWAPPER = insightface.model_zoo.get_model(model_path, providers=['CPUExecutionProvider'])
     return FACE_SWAPPER
 
 
